ffe14d1693fa215d02b1deba3c02ce1f455780f2
Zyphyriuz <dam20062017@gmail.com>
2025-05-23 15:29:01 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+// this calls lazada.js to use the functions
+const lazada = require('./lazada.js');
+
+// Add a product, will also return name for so we know the change in name later
+console.log(lazada.addProduct("202", "Mini Plush", 15, 30));
+
+// Update a product, 
+console.log(lazada.updateProduct("202", "Slightly Minier Plush", 13, 27));
+
+// Delete a product using the ID
+console.log(lazada.deleteProduct("202"));
+
+// Get a specific product using the ID too
+console.log(lazada.getProductID("198"));
+
+// List all products 
+console.log(lazada.getallProducts());
